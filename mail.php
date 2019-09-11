0b7501d0e1b2f9b9edec851b8d5a8b3d711a22eb
MephistoEd <mephistoeditor@gmail.com>
2019-09-11 15:40:53 +0300

    php test 2

--- mail.php
+++ mail.php
@@ -3,18 +3,18 @@
 if(isset($_POST['submit'])){
 $to = "mephistoeditor@gmail.com";; 
 $from = $_POST['email']; 
-$first_name = $_POST['first_name'];
+$name = $_POST['name'];
 $subject = "Форма отправки сообщений с сайта";
 $subject2 = "Copy of your form submission";
-$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
-$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+$message = $name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
 $headers = "From:" . $from;
 $headers2 = "From:" . $to;
 
 mail($to,$subject,$message,$headers);
 // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
 echo "<br /><br /><a href='https://gorlenkov.tk'>Вернуться на сайт.</a>";
 
 }
