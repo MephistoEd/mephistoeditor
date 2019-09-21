c591aa0ac8a55093a06c6a52653ae775e71abe04
MephistoEd <mephistoeditor@gmail.com>
2019-09-21 16:12:57 +0300

    del

--- assets/js/button.js
+++
@@ -1,7 +0,0 @@
-function facechange (objName) {
- if ( $(objName).css('display') == 'none' ) {
- $(objName).animate({height: 'show'}, 400);
- } else {
- $(objName).animate({height: 'hide'}, 200);
- }
-};
