235807d1521bac9a700722f76414defefb0c5302
MephistoEd <mephistoeditor@gmail.com>
2019-09-21 14:49:02 +0300

    v.2.6.5.G

---
+++ assets/js/button.js
@@ -0,0 +1,7 @@
+function facechange (objName) {
+ if ( $(objName).css('display') == 'none' ) {
+ $(objName).animate({height: 'show'}, 400);
+ } else {
+ $(objName).animate({height: 'hide'}, 200);
+ }
+};
